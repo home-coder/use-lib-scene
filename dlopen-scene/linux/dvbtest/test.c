72876c4e168556c28045a91f93b8013a1d2ac439
jiangxiujie <jiangxiujie@cantv.cn>
2017-07-19 20:06:40 +0800

    Linux：还没有编译通过，需要Makefile的知识,正在修改...

---
+++ dlopen-scene/linux/dvbtest/test.c
@@ -0,0 +1,13 @@
+int test_open()
+{
+	printf("test_open\n");
+	return 0;
+}
+
+int test_contrl()
+{
+	printf("test_contrl\n");
+	//ioctl(int fd, unsigned int cmd, unsigned long arg)
+	return 0;
+}
+
---
+++ dlopen-scene/linux/include/test.h
@@ -0,0 +1,7 @@
+#ifndef _TEST_H
+#define _TEST_H
+
+int test_open();
+int test_contrl();
+
+#endif
